c17bc0c762bd647054d9882f54df334a09aff753
hughesblues94 <hughesblues94@gmail.com>
2022-11-29 21:39:55 +0000

    added a navbar with react router. Added minor CSS to link elements

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,17 @@
 import '../Styles/app.css';
+import AddProperty from './AddProperty';
 import Navbar from './Navbar';
+import Properties from './Properties';
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Properties />} />
+        <Route path='/add-property' element={<AddProperty />} />
+      </Routes>
     </div>
   );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,14 +1,16 @@
 import '../Styles/navbar.css'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
         <div className='navbar'>
             <ul className='navbar-links'>
                 <img className="logo" alt="logo" src={require('../Styles/logo.png')} />
-                <li className='navbar-links-item'>View Properties</li>
-                <li className='navbar-links-item'>Add a Property</li>
+
+                <Link className="navbar-links-item" to="/">View Properties</Link>
+                <Link className="navbar-links-item" to="/add-property">Add Properties</Link>
             </ul>
-        </div>
+        </div >
     );
 }
 
